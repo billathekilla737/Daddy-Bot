84f72de1f4aa8f7b093151984ef8261c9cf3cbfd
Zack <zacharygarris737@gmail.com>
2024-03-20 10:59:32 -0500

    Removed Melee Reminders

--- Daddy-Bot-env/DaddyBot.py
+++ Daddy-Bot-env/DaddyBot.py
@@ -1,11 +1,10 @@
 from Assets.F1_Functions import *
-from Assets.Melee_Functions import *
 from Assets.pStar_Names import *
 from Assets.FlatFuckFriday import *
 from Assets.Support import *
 from discord import app_commands # type: ignore
 from discord.ext import commands # type: ignore
-import pytz
+#import pytz
 from tabulate import tabulate
 from datetime import datetime, timedelta
 
@@ -30,7 +29,6 @@ def run_discord_bot():
     #scrape_race_info()                                          #
     token, URL = Parse_Private()                                #
     Scrap_Names()                                               #
-    Scrap_Melee()                                               #
     scrape_f1_races('https://f1calendar.com')
     intents = discord.Intents.all()                             #
     global sent                                                 #
@@ -55,8 +53,6 @@ def run_discord_bot():
         PrevEvent = "" 
         # Event = find_next_event(RacesJson) # type: ignore
         # days, hours, minutes = get_timedelta_to_next_event(RacesJson)
-        MeleeEvent, *_ = find_Next_Major()
-        PrevMeleeEvent = ""
         channel = client.get_channel(907764974099787797)
         #meleechannel = client.get_channel(1117158502989844600)
         meleechannel = client.get_channel(1117158502989844600)
@@ -93,40 +89,47 @@ def run_discord_bot():
             timedelta_str = f"{days} days, {hours}:{minutes:02d} hours from now"
             if shouldSendF1Reminder and PrevEvent != next_event['event_type']:
 
-                if "Free Practice" in next_event['event_type']:
+                event_type = next_event['event_type']
+                if "Free Practice" in event_type:
                     FreePracticeRole = discord.utils.get(client.guilds[0].roles, name="Free Practice")
-                    await channel.send(f"{FreePracticeRole.mention} **{next_event['event_type']}** is in " + str(hours) + " hours and " + str(minutes) + " minutes!")
-                    PrevEvent = next_event['event_type']
+                    await channel.send(f"{FreePracticeRole.mention} **{event_type}** is in {hours} hours and {minutes} minutes!")
+                    PrevEvent = event_type
 
-                #TODO: ADD SPRINT AND SPRIINT SHOOTOUT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                #TODO: Make TimeDelta Event Based
-                
+                elif "Sprint Shootout" in event_type:
+                    SprintShootoutRole = discord.utils.get(client.guilds[0].roles, name="Sprint Shootout")
+                    await channel.send(f"{SprintShootoutRole.mention} **{event_type}** is in {hours} hours and {minutes} minutes!")
+                    PrevEvent = event_type
 
-                elif "Qualifying" in next_event['event_type'] or "Sprint" in next_event['event_type']:
+                elif "Sprint" in event_type:
+                    SprintRole = discord.utils.get(client.guilds[0].roles, name="Sprint")
+                    await channel.send(f"{SprintRole.mention} **{event_type}** is in {hours} hours and {minutes} minutes!")
+                    PrevEvent = event_type
+
+                elif "Qualifying" in event_type or "Sprint" in event_type:
                     QualifyingRole = discord.utils.get(client.guilds[0].roles, name="Qualifying")
-                    await channel.send(f"{QualifyingRole.mention} **{next_event['event_type']}** is in " + str(hours) + " hours and " + str(minutes) + " minutes!")
-                    PrevEvent = next_event['event_type']
+                    await channel.send(f"{QualifyingRole.mention} **{event_type}** is in {hours} hours and {minutes} minutes!")
+                    PrevEvent = event_type
                              
-                elif ("Grand Prix Grand Prix" in next_event['event_type']):
-                    GrandPrix = discord.utils.get(client.guilds[0].roles, name="Grand Prix")
-                    PrevEvent = next_event['event_type']
-                    next_event['event_type'] = next_event['event_type'][:-10]
-                    await channel.send(f"{GrandPrix.mention} **{next_event['event_type']}** is in " + str(hours) + " hours and " + str(minutes) + " minutes!")
+                elif "Grand Prix Grand Prix" in event_type:
+                    GrandPrixRole = discord.utils.get(client.guilds[0].roles, name="Grand Prix")
+                    PrevEvent = event_type
+                    event_type = event_type.replace(" Grand Prix", "")
+                    await channel.send(f"{GrandPrixRole.mention} **{event_type}** is in {hours} hours and {minutes} minutes!")
                     
 
-            #Melee Reminder
+            #Melee Reminder (SHUT DOWN UNTIL A Better Site To Scrap Is Found)
             #################################################################
-            try:
-                shouldSendMeleeReminder = isMeleeTime()
-            except Exception as e:
-                print(f"{e}  Error in isMeleeTime()")
-                shouldSendMeleeReminder = False
+            # try:
+            #     shouldSendMeleeReminder = isMeleeTime()
+            # except Exception as e:
+            #     print(f"{e}  Error in isMeleeTime()")
+            #     shouldSendMeleeReminder = False
             
-            if shouldSendMeleeReminder and PrevMeleeEvent != MeleeEvent:
-                MeleeRole = discord.utils.get(client.guilds[0].roles, name="Melee")
-                #Message = f"{role.mention} {Event} is in {TimeDelta}!"
-                await meleechannel.send(f"{MeleeRole.mention} {MeleeEvent} is Today!")
-                PrevMeleeEvent = MeleeEvent
+            # if shouldSendMeleeReminder and PrevMeleeEvent != MeleeEvent:
+            #     MeleeRole = discord.utils.get(client.guilds[0].roles, name="Melee")
+            #     #Message = f"{role.mention} {Event} is in {TimeDelta}!"
+            #     await meleechannel.send(f"{MeleeRole.mention} {MeleeEvent} is Today!")
+            #     PrevMeleeEvent = MeleeEvent
 
             #Flat Fuck Friday Reminder
             ################################################################
@@ -218,7 +221,7 @@ def run_discord_bot():
         nextevent = find_next_event(RacesJson)
         await interaction.response.send_message(f"The next F1 event is **{nextevent['event_type']}** on **{nextevent['date']} at {convert_to_12hr(nextevent['time'])}** <a:max_nice:1117178831120371824> \n T-minus {timedelta_str} until the next event!")
     #############################################################################################################################################
-    #TODO FIX WEEK COMMAND
+    
     @tree.command(name = "week", description = "Tells you the next F1 events for the week <:f1_logo:1132150006988673034>")
     async def week(interaction: discord.Interaction):
         NextFP1                 = find_next_event_by_type(RacesJson, "Free Practice 1")
@@ -227,7 +230,7 @@ def run_discord_bot():
         NextQualifying          = find_next_event_by_type(RacesJson, "Qualifying")
         NextSprintShootout      = find_next_event_by_type(RacesJson, "Sprint Shootout")
         NextSprint              = find_next_event_by_type(RacesJson, "Sprint")
-        NextGrandPrix           = find_next_event_by_type(RacesJson, "Grand Prix")
+        NextGrandPrix           = find_next_event_by_type(RacesJson, "Grand Prix Grand Prix")
         Next_Location           = find_next_event(RacesJson)['event_type']
         pattern = r"^(.*?Grand Prix)"
         match = re.search(pattern, Next_Location)
@@ -287,23 +290,24 @@ def run_discord_bot():
 
     #Reaction Roles
     ##############################################################################################################################################
-    @client.event
-    async def on_raw_reaction_add(payload):
-        meleechannel = client.get_channel(1117158502989844600)
-        if payload.emoji.name == "🥊" and payload.message_id == 1119503042144911440 and payload.channel_id == meleechannel.id:
-            guild = await client.fetch_guild(payload.guild_id)
-            member = await guild.fetch_member(payload.user_id)
-            role = discord.utils.get(guild.roles, name="Melee")
-            await member.add_roles(role)
+    #Shutting Down Melee Reaction Roles for now
+    #@client.event
+    # async def on_raw_reaction_add(payload):
+    #     meleechannel = client.get_channel(1117158502989844600)
+    #     if payload.emoji.name == "🥊" and payload.message_id == 1119503042144911440 and payload.channel_id == meleechannel.id:
+    #         guild = await client.fetch_guild(payload.guild_id)
+    #         member = await guild.fetch_member(payload.user_id)
+    #         role = discord.utils.get(guild.roles, name="Melee")
+    #         await member.add_roles(role)
 
-    @client.event
-    async def on_raw_reaction_remove(payload):
-        meleechannel = client.get_channel(1117158502989844600)
-        if payload.emoji.name == "🥊" and payload.message_id == 1119503042144911440 and payload.channel_id == meleechannel.id:
-            guild = await client.fetch_guild(payload.guild_id)
-            member = await guild.fetch_member(payload.user_id)
-            role = discord.utils.get(guild.roles, name="Melee")
-            await member.remove_roles(role)
+    # @client.event
+    # async def on_raw_reaction_remove(payload):
+    #     meleechannel = client.get_channel(1117158502989844600)
+    #     if payload.emoji.name == "🥊" and payload.message_id == 1119503042144911440 and payload.channel_id == meleechannel.id:
+    #         guild = await client.fetch_guild(payload.guild_id)
+    #         member = await guild.fetch_member(payload.user_id)
+    #         role = discord.utils.get(guild.roles, name="Melee")
+    #         await member.remove_roles(role)
     #Misc.
     ##############################################################################################################################################
     async def reset_sent():
@@ -313,15 +317,6 @@ def run_discord_bot():
         print("Reset for FFF sent")
         
     
-
-
-
-
-
-
-
-
-
 
     ####Initilize the bot###
                            #
